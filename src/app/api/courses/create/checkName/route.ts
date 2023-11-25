3359231618a85f871e3f285190e05116e0ecfe76
Keeghan McGarry <keeghan_m@live.com>
2023-11-25 18:57:49 +0000

    Missed one for global prisma

--- src/app/api/courses/create/checkName/route.ts
+++ src/app/api/courses/create/checkName/route.ts
@@ -1,7 +1,5 @@
 import { errorResponse, successResponse } from "~/app/api/responses";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import { prisma } from "~/server/db";
 
 export async function POST(request: Request) {
   const { name } = await request.json();
