b6d4562ba79a203d69e116aa18689d145c75b455
Keeghan McGarry <keeghan_m@live.com>
2023-12-13 10:40:56 +0000

    Build Erros

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,9 +1,3 @@
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export async function GET(
-  request: Request,
-  { params }: { params: { kindeAuth: string } },
-) {
-  const endpoint = params.kindeAuth;
-  return handleAuth(request, endpoint);
-}
+export const GET = handleAuth();
