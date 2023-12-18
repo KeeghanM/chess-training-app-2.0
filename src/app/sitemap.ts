09a6d4ee8399d296c91af901f551d5c217eeab62
Keeghan McGarry <keeghan_m@live.com>
2023-12-18 14:45:56 +0000

    Custom Sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,97 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const pages: MetadataRoute.Sitemap = [
+    {
+      url: 'https://chesstraining.app',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: 'https://chesstraining.app/about',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.7,
+    },
+    {
+      url: 'https://chesstraining.app/about/meet-the-team',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.7,
+    },
+    {
+      url: 'https://chesstraining.app/about/features',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: 'https://chesstraining.app/about/features/natural-play-learning',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.9,
+    },
+    {
+      url: 'https://chesstraining.app/about/features/woodpecker-method',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.9,
+    },
+    {
+      url: 'https://chesstraining.app/contact',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.7,
+    },
+    {
+      url: 'https://chesstraining.app/contact/report-an-issue',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://chesstraining.app/courses',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.9,
+    },
+    {
+      url: 'https://chesstraining.app/product-roadmap',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://chesstraining.app/training/tactics',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.9,
+    },
+    {
+      url: 'https://chesstraining.app/training/endgames',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.9,
+    },
+    {
+      url: 'https://chesstraining.app/training/knight-vision',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+    {
+      url: 'https://chesstraining.app/training/visualisation',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+  ]
+
+  pages.sort((a, b) => {
+    // sort by priority order, highest first
+    return (b.priority ?? 0.5) - (a.priority ?? 0.5)
+  })
+
+  return pages
+}
