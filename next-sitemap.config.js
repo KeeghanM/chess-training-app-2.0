8cf8d061b1a023260619a1145cec30714616b4a1
Keeghan McGarry <keeghan_m@live.com>
2023-12-18 12:25:36 +0000

    Non Module SItemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
-module.exports = {
+const config = {
   siteUrl: process.env.SITE_URL || 'https://chesstraining.app',
   generateRobotsTxt: true,
   exclude: ['/api/*', '/_next/*', '/_error/*', '/dashboard/*'],
 }
+
+export default config
