14e00098d0d20e4bddb2269d03f1c1a3e94053cf
Keeghan McGarry <keeghan_m@live.com>
2023-12-31 16:57:15 +0000

    COrrection to streak training

--- src/app/_util/UpdateStreak.ts
+++ src/app/_util/UpdateStreak.ts
@@ -12,21 +12,24 @@ export async function UpdateStreak(userId: string) {
     if (!profile) throw new Error('Profile not found')
 
     const currentDate = new Date()
-    const lastTrained = profile.lastTrained ? profile.lastTrained : new Date()
-    const timeSinceLastIncrement = currentDate.getTime() - lastTrained.getTime()
+    const lastIncrement = profile.lastIncrement
+      ? profile.lastIncrement
+      : new Date()
+    const timeSinceLastIncrement =
+      currentDate.getTime() - lastIncrement.getTime()
     const oneDay = 1000 * 60 * 60 * 24
 
     let currentStreak = profile.currentStreak
 
     const yesterday = new Date(currentDate)
     yesterday.setDate(yesterday.getDate() - 1)
-    const isTrainingYesterday =
-      lastTrained.toDateString() === yesterday.toDateString()
+    const didTrainYesterday =
+      lastIncrement.toDateString() === yesterday.toDateString()
 
     if (
       (timeSinceLastIncrement >= oneDay &&
         timeSinceLastIncrement < oneDay * 2) ||
-      isTrainingYesterday
+      didTrainYesterday
     ) {
       currentStreak++
       const bestStreak = Math.max(currentStreak, profile.bestStreak)
@@ -34,6 +37,7 @@ export async function UpdateStreak(userId: string) {
       await prisma.userProfile.update({
         where: { id: userId },
         data: {
+          lastIncrement: new Date(),
           lastTrained: new Date(),
           currentStreak,
           bestStreak,
@@ -50,6 +54,18 @@ export async function UpdateStreak(userId: string) {
         // Add the badge to the user
         await AddBadgeToUser(userId, badge.name)
       }
+    } else {
+      const lastTrained = profile.lastTrained ? profile.lastTrained : new Date()
+      const timeSinceLastTrained = currentDate.getTime() - lastTrained.getTime()
+
+      await prisma.userProfile.update({
+        where: { id: userId },
+        data: {
+          lastTrained: new Date(),
+          currentStreak:
+            timeSinceLastTrained >= oneDay ? 1 : profile.currentStreak,
+        },
+      })
     }
 
     return
