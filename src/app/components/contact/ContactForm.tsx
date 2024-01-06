c40104319f7c25c4c649c00bc15a7c02cdce0253
Keeghan McGarry <keeghan_m@live.com>
2024-01-06 13:05:11 +0000

    Better Input Styles

--- src/app/components/contact/ContactForm.tsx
+++ src/app/components/contact/ContactForm.tsx
@@ -132,7 +132,7 @@ export default function ContactForm() {
                 <div>
                   <label>Name</label>
                   <input
-                    className="w-full border border-gray-300 px-4 py-2"
+                    className="w-full border border-gray-300 px-4 py-2 bg-gray-100 text-black"
                     type="text"
                     value={name}
                     onChange={(e) => setName(e.target.value)}
@@ -142,7 +142,7 @@ export default function ContactForm() {
                 <div>
                   <label>Email</label>
                   <input
-                    className="w-full border border-gray-300 px-4 py-2"
+                    className="w-full border border-gray-300 px-4 py-2 bg-gray-100 text-black"
                     type="email"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
