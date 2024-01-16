731ef3d4e0253d51e53927456b2b5f785fbcd131
Keeghan McGarry <keeghan_m@live.com>
2024-01-16 17:36:08 +0000

    Direct to login page

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,41 +1,10 @@
 import { redirect } from 'next/navigation'
 
-import {
-  LoginLink,
-  RegisterLink,
-} from '@kinde-oss/kinde-auth-nextjs/components'
-
-import Button from '~/app/components/_elements/button'
-import Container from '~/app/components/_elements/container'
-import Heading from '~/app/components/_elements/heading'
-
 import { getUserServer } from '~/app/_util/getUserServer'
 
 export default async function SignIn() {
   const { user } = await getUserServer()
 
   if (user) redirect('/dashboard')
-
-  return (
-    <Container>
-      <div className="flex flex-col gap-4">
-        <Heading as={'h1'}>Sign In or Register</Heading>
-        <p>
-          To get the most out of ChessTraining.app, and to access all of our
-          features, please sign in or register. Registration is{' '}
-          <span className="text-orange-500">completely free</span>, and you'll
-          get access to every single one of our Science Backed training tools
-          forever.
-        </p>
-        <div className="flex flex-wrap gap-4 md:flex-row">
-          <LoginLink postLoginRedirectURL="/dashboard">
-            <Button variant="primary">Sign in</Button>
-          </LoginLink>
-          <RegisterLink postLoginRedirectURL="/dashboard/new">
-            <Button variant="accent">Register</Button>
-          </RegisterLink>
-        </div>
-      </div>
-    </Container>
-  )
+  else redirect(`/api/auth/login?post_login_redirect_url=/dashboard`)
 }
