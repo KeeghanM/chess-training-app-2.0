f730a6ce870938aa8baba8f33326cae8278aa464
Keeghan McGarry <keeghan_m@live.com>
2024-01-30 23:04:52 +0000

    Recall SEO

--- src/app/training/recall/page.tsx
+++ src/app/training/recall/page.tsx
@@ -1,41 +1,98 @@
+import Link from 'next/link'
+
+import Button from '~/app/components/_elements/button'
+import Heading from '~/app/components/_elements/heading'
+import StyledLink from '~/app/components/_elements/styledLink'
 import CtaRow from '~/app/components/_layouts/ctaRow'
-import PageHeader from '~/app/components/_layouts/pageHeader'
+import Hero from '~/app/components/_layouts/hero'
+import ImageRowFull from '~/app/components/_layouts/imageRowFull'
 
 export const metadata = {
-  title: 'Recall Training - Advance Your Chess Strategy at ChessTraining.app',
+  title: 'Recall Trainer - Learn Chess Online with ChessTraining.app',
   description:
-    "Improve your board vision, and memory skills with ChessTraining.app's Recall training. Designed to test your memory and understanding of the board and it's pieces. Perfect for those looking to elevate their game to the next level.",
+    "Improve your chess skills with ChessTraining.app's Recall Trainer. Designed for busy adults, our trainer helps you learn chess efficiently and effectively. Build your memory and visualize chess positions. Enhance your chess skill development with structured training routines. Start learning chess online today!",
 }
 
-export default async function RecallPage() {
+export default async function Recall() {
   return (
     <>
-      <PageHeader
-        title="Recall Training"
-        subTitle="Coming soon!"
+      <Hero
+        title="Recall Trainer - Learn Chess Online"
         image={{
           src: '/images/hero.avif',
-          alt: 'Wooden chess pieces on a chess board',
+          alt: 'Chess pieces set up on a chessboard',
         }}
-      />
-      <CtaRow
-        title="Curious?"
-        background="light"
         cta={{
-          text: 'View our roadmap',
-          link: '/product-roadmap',
+          text: 'Start Recalling',
+          link: '/training/recall/train',
         }}
       >
-        <div className="flex flex-col gap-2">
-          <p>
-            We're working hard to bring you this feature as soon as possible. If
-            you want to know more about our upcoming features, check out our
-            roadmap.
-          </p>
-          <p className="italic">
-            P.s You can also see all our other upcoming feature releases.
-          </p>
-        </div>
+        <Heading color="text-orange-500" as="h2">
+          Enhance Your Chess Memory and Position Visualization
+        </Heading>
+      </Hero>
+      <ImageRowFull
+        heading="How to Use the Recall Trainer"
+        background="light"
+        image={{
+          src: '/images/chunking.avif',
+          alt: 'A chessboard with pieces highlighted in the Carlsbad Structure.',
+        }}
+        imageSide="right"
+      >
+        <p>
+          Our Recall Trainer is the perfect tool to improve your memory and
+          visualization skills in chess. Designed for busy adults like you, it
+          offers efficient and effective training tailored to your chess rating.
+        </p>
+        <p>
+          With this trainer, you'll be presented with chess positions for a
+          brief moment, or longer – you decide. Your task is to recall specific
+          details about the position. It forces you to break up the board into
+          smaller, more easily recognizable positions, improving your memory and
+          chess vision.
+        </p>
+        <p>
+          Our trainer simplifies the process and automates tracking, so you can
+          focus solely on enhancing your chess memory and visualization. Whether
+          you have a few minutes or an hour, our Recall Trainer fits your
+          schedule.
+        </p>
+      </ImageRowFull>
+      <ImageRowFull
+        heading="Why Recall Training Matters"
+        background="light"
+        image={{
+          src: '/images/woman_kneeling_at_chess.png',
+          alt: 'A woman studying chess positions',
+        }}
+        imageSide="left"
+      >
+        <p>
+          Chess is all about recognizing patterns and positions. The ability to
+          recall critical details about a position can be a game-changer. Our
+          Recall Trainer helps you build this essential skill, ensuring that you
+          can visualize chess positions with ease.
+        </p>
+        <p>
+          You can customize the difficulty level by adjusting the viewing time,
+          the number of pieces on the board, and the number of pieces you need
+          to recall. It's flexible training designed to meet your specific
+          needs.
+        </p>
+      </ImageRowFull>
+      <CtaRow
+        title="Ready to Boost Your Chess Memory?"
+        cta={{
+          text: 'Start Recalling',
+          link: '/training/recall/train',
+        }}
+        background="dark"
+      >
+        <p>
+          Improve your memory and visualization skills to enhance your chess
+          game. Begin your recall training journey today.
+        </p>
       </CtaRow>
     </>
   )
