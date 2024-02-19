49846c38999d25085729a36b72d467f5cdc04c0a
Keeghan McGarry <keeghan_m@live.com>
2024-02-19 16:11:13 +0000

    Article Sort

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -43,19 +43,26 @@ export default async function ArticlesPage() {
           Read the latest articles on chess improvement and chess training.
           Learn how to improve your chess game and become a better chess player.
         </p>
-        {articles.map((article) => {
-          const author = article.data.author as ContentRelationshipField & {
-            data: { name: string; uid: string }
-          }
-          return (
-            <div
-              className="flex flex-col gap-0 mb-4 border border-gray-300 shadow-md bg-[rgba(0,0,0,0.03)]  hover:shadow-lg transition-shadow duration-300"
-              key={article.id}
-            >
-              <script
-                type="application/ld+json"
-                dangerouslySetInnerHTML={{
-                  __html: `{
+        {articles
+          .sort((a, b) => {
+            return (
+              new Date(b.first_publication_date).getTime() -
+              new Date(a.first_publication_date).getTime()
+            )
+          })
+          .map((article) => {
+            const author = article.data.author as ContentRelationshipField & {
+              data: { name: string; uid: string }
+            }
+            return (
+              <div
+                className="flex flex-col gap-0 mb-4 border border-gray-300 shadow-md bg-[rgba(0,0,0,0.03)]  hover:shadow-lg transition-shadow duration-300"
+                key={article.id}
+              >
+                <script
+                  type="application/ld+json"
+                  dangerouslySetInnerHTML={{
+                    __html: `{
                     "@context": "https://schema.org",
                     "@type": "Article",
                     "headline": "${article.data.title}",
@@ -80,57 +87,57 @@ export default async function ArticlesPage() {
                     "description": "${asText(article.data.introduction)}"
                     }
                 `,
-                }}
-              ></script>
-              <div className="px-2 py-1 border-b border-gray-300 font-bold text-orange-500">
-                <Link
-                  className="hover:underline"
-                  href={`/articles/${article.uid}`}
-                >
-                  <h2>{article.data.title}</h2>
+                  }}
+                ></script>
+                <div className="px-2 py-1 border-b border-gray-300 font-bold text-orange-500">
+                  <Link
+                    className="hover:underline"
+                    href={`/articles/${article.uid}`}
+                  >
+                    <h2>{article.data.title}</h2>
+                  </Link>
+                </div>
+                <div className="flex flex-wrap gap-2 pt-2 justify-center text-xs">
+                  <div className="flex flex-col items-center border border-gray-300">
+                    <p className="font-bold py-1 px-2 border-b border-gray-300 w-full text-center">
+                      Author
+                    </p>
+                    <p className="p-1">
+                      <Link
+                        href={`/articles/author/${author.data.uid}`}
+                        className="hover:no-underline text-purple-700 underline"
+                      >
+                        {author.data.name}
+                      </Link>
+                    </p>
+                  </div>
+                  <div className="flex flex-col items-center border border-gray-300">
+                    <p className="font-bold py-1 px-2 border-b border-gray-300 w-full text-center">
+                      Published
+                    </p>
+                    <p className="p-1">
+                      {new Date(
+                        article.first_publication_date,
+                      ).toLocaleDateString('en-GB', {
+                        year: 'numeric',
+                        month: 'long',
+                        day: 'numeric',
+                      })}
+                    </p>
+                  </div>
+                </div>
+                <div
+                  className="p-2"
+                  dangerouslySetInnerHTML={{
+                    __html: asHTML(article.data.introduction),
+                  }}
+                />
+                <Link className="p-2 mx-auto" href={`/articles/${article.uid}`}>
+                  <Button variant="primary">Read More</Button>
                 </Link>
               </div>
-              <div className="flex flex-wrap gap-2 pt-2 justify-center text-xs">
-                <div className="flex flex-col items-center border border-gray-300">
-                  <p className="font-bold py-1 px-2 border-b border-gray-300 w-full text-center">
-                    Author
-                  </p>
-                  <p className="p-1">
-                    <Link
-                      href={`/articles/author/${author.data.uid}`}
-                      className="hover:no-underline text-purple-700 underline"
-                    >
-                      {author.data.name}
-                    </Link>
-                  </p>
-                </div>
-                <div className="flex flex-col items-center border border-gray-300">
-                  <p className="font-bold py-1 px-2 border-b border-gray-300 w-full text-center">
-                    Published
-                  </p>
-                  <p className="p-1">
-                    {new Date(
-                      article.first_publication_date,
-                    ).toLocaleDateString('en-GB', {
-                      year: 'numeric',
-                      month: 'long',
-                      day: 'numeric',
-                    })}
-                  </p>
-                </div>
-              </div>
-              <div
-                className="p-2"
-                dangerouslySetInnerHTML={{
-                  __html: asHTML(article.data.introduction),
-                }}
-              />
-              <Link className="p-2 mx-auto" href={`/articles/${article.uid}`}>
-                <Button variant="primary">Read More</Button>
-              </Link>
-            </div>
-          )
-        })}
+            )
+          })}
       </Container>
     </>
   )
