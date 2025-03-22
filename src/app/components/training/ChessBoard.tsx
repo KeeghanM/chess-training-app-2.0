6db53b068b06d615a58701751566e508d36cd448
Keeghan McGarry <keeghan_m@live.com>
2025-03-22 14:31:21 +0000

    refactor(chessboard.tsx): added not null to effect

--- src/app/components/training/ChessBoard.tsx
+++ src/app/components/training/ChessBoard.tsx
@@ -187,7 +187,7 @@ export default function ChessBoard(props: ChessBoardProps) {
   }
 
   useEffect(() => {
-    if (!startSquare || !clickedPiece) {
+    if (!game || !startSquare || !clickedPiece) {
       setOptionSquares({})
       return
     }
@@ -206,8 +206,8 @@ export default function ChessBoard(props: ChessBoardProps) {
     validMoves.map((move) => {
       newOptions[move.to] = {
         background:
-          game!.get(move.to) &&
-          game!.get(move.to).color !== game.get(startSquare).color
+          game.get(move.to) &&
+          game.get(move.to).color !== game.get(startSquare).color
             ? 'radial-gradient(circle, transparent 50%,  rgba(0, 0, 0, 0.2) 51%,  rgba(0, 0, 0, 0.2) 65%,transparent 66%)'
             : 'radial-gradient(circle, rgba(0,0,0,.2) 20%, transparent 22%)',
         borderRadius: '50%',
